f843227477d9fb58ae107c1610a5449e93e2341b
mmrobin <malachi3@illinois.edu>
2024-07-27 19:56:57 -0500

    Edited js

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 import * as d3 from "https://d3js.org/d3.v7.min.js";
 
+console.log("Is this gonna fuckin work?");
+
 console.log("Retrieving dataset as csv");
 // import both datasets from csv files
 d3.csv("/data/Coffee_import.csv", function(data){
